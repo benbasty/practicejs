6746f34030fa71086db4977ffe3217ce4ef5a409
benbasty <benbasty@yahoo.fr>
2023-11-11 17:37:50 +0800

    plain javascript practice

--- practicejs.js
+++ practicejs.js
@@ -70,3 +70,90 @@ const id = setInterval(() => {
     console.log("Hey There")
 }, 3000);
 clearInterval(id);
+
+// reduce method
+// take some array and reduce it to a single value
+// 2 parametors
+// accumulator, currentvalue
+
+const arr = [1, 3, 2, 8, 9, 5, 4, 2];
+const total = arr.reduce((total, elem) =>
+    total + elem);
+console.log(total);
+
+// find minimum value
+const min = arr.reduce((min,elem) => {
+    if(elem < min) {
+        return min;
+    }
+    return min;
+});
+console.log(min);
+
+// default params
+
+function rollDie(numSides = 8) {
+    return Math.floor(Math.random() * numSides) + 1;
+}
+console.log(rollDie());
+
+// spread in function call
+
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+console.log(...numbers);
+
+// spread in array litterals
+const drinks = ['cocacola','sprite','soda'];
+const coffee = ['cappucino', 'americano','latte'];
+const beverages = [...drinks, ...coffee];
+console.log(beverages);
+
+// spread with object litterals
+const movie = {
+    title: 'marvels',
+    length: 120,
+    rating: '5stars',
+    release: 2023
+}
+
+const movie2 = {...movie, id:1, comment:'awesome'};
+console.log(movie2);
+
+// rest params
+
+function allNums(...nums) {
+    return nums.reduce((all,el) => all + el);
+}
+console.log(allNums(1,2,3,4));
+
+
+// destructuring arrays
+
+const scores = [1,2,8,6,9,4,7,12,14,10,11];
+
+const [first,second,third, ...restNumbers] = scores;
+console.log(restNumbers);
+
+// destructuring obects
+
+const user = {
+    email: 'beno@gmail.com',
+    password: 'abcd1234567890',
+    firstName: 'ben',
+    lastName: 'BOROTO',
+    born: 1987,
+    bio: 'a clever boy',
+    city: 'San Antonio',
+    state: 'Cacalidor'
+}
+
+// destructuring method here consists in renaming each parameters with a const name
+const {email, firstName, lastName, city, bio} = user;
+console.log(email);
+// renaming born to birthYear
+const {born: birthYear} = user;
+console.log(birthYear);
+
+// destructuring params
+
+
